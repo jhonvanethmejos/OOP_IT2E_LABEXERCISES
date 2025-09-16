a8c0210a6070e3206e74c9de6e905654bf2d03a0
Jhon Vaneth Mejos <jhonvaneth.mejos@dorsu.edu.ph>
2025-09-16 22:38:31 +0800

    Update ShapeTester.java

--- EXER_3_MEJOS/ShapeTester.java
+++ EXER_3_MEJOS/ShapeTester.java
@@ -1,16 +1,16 @@
 public class ShapeTester{
     public static void main(String[] args) {
         
-        Shape circle = new Circle(5.0f);
+        Shape circle = new Circle(8.0f);
         System.out.println(circle.DisplayInfo());  
 
-        Shape rectangle = new Rectangle(4.0f, 6.0f);
+        Shape rectangle = new Rectangle(5.0f, 7.0f);
         System.out.println(rectangle.DisplayInfo());
 
-        Shape square = new Square(4.0f);
+        Shape square = new Square(9.0f);
         System.out.println(square.DisplayInfo());
 
-        Shape triangle = new Triangle(3.0f, 4.0f, 5.0f);
+        Shape triangle = new Triangle(2.0f, 3.0f, 4.0f);
         System.out.println(triangle.DisplayInfo());
 
     }
